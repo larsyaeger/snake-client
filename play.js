236260f363d2decee4d4d21ac1b1a5bb0f7ea394
larsyaeger <larsyaeger@gmail.com>
2022-11-23 14:55:56 -0700

    now accepting userinput in the form of control+c to exit the program

--- play.js
+++ play.js
@@ -1,5 +1,19 @@
 const net = require('net');
 const connect = require('./client');
-
 console.log('connecting ...');
 connect();
+const handleUserInput = (data) => {
+	//console.log(data);
+	if (data === '\u0003') {
+		process.exit(0);
+	};
+};
+const setupInput = () => {
+	const stdin = process.stdin;
+	stdin.setRawMode(true);
+	stdin.setEncoding('utf8');
+	stdin.resume();
+	stdin.on('data', handleUserInput);
+	return stdin;
+};
+setupInput();
