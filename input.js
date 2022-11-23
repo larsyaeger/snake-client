27c49e5e16c86e91c7783f7712b9af0ed4ac8d1a
larsyaeger <larsyaeger@gmail.com>
2022-11-23 15:39:51 -0700

    made my snake able to move

--- input.js
+++ input.js
@@ -1,10 +1,20 @@
+let connection;
 const handleUserInput = (data) => {
 	//console.log(data);
 	if (data === '\u0003') {
 		process.exit(0);
-	};
+	} else if (data === 'w') {
+		connection.write('Move: up');
+	} else if (data === 'a') {
+		connection.write('Move: left');
+	} else if (data === 's') {
+		connection.write('Move: down');
+	} else if (data === 'd') {
+		connection.write('Move: right');
+	}
 };
-const setupInput = () => {
+const setupInput = (conn) => {
+	connection = conn;
 	const stdin = process.stdin;
 	stdin.setRawMode(true);
 	stdin.setEncoding('utf8');
